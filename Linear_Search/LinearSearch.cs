b9342d648dfc95e4ab6bdc4811577e68ad73921f
Ezekiela <ezekielaanderson@.com>
2021-06-17 08:19:10 -0700

    Linear search first commit

---
+++ Linear_Search/LinearSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linear_Search
+{
+    class LinearSearch
+    {
+        public LinearSearch(){}
+        public void Reorder(int [] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+            }
+            for (int i = 0; i < a.Length/2;i++ ) {
+                int temp = a[i];
+                a[i] = a[a.Length - i - 1];
+                a[a.Length - i - 1] = temp;
+                Console.WriteLine(a[i]);
+            }
+            for (int i = 0; i < a.Length; i++) {
+                Console.Write(a[i]);
+            }
+        }
+        public int[,] Matrixmultiplication(int[,] a, int[,] b) {
+            int[,] c=new int[a.GetLength(0),a.GetLength(0)];
+            for (int i = 0; i < a.GetLength(0); i++) {
+                for (int j = 0; j < a.GetLength(0); j++) {
+                    c[i, j] = 0;
+                    for (int k = 0; k < a.GetLength(0); k++)
+                        c[i, j] += a[i, k] * b[k, j];
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ Linear_Search/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearSearch l = new LinearSearch();
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            l.Reorder(a);
+            Console.ReadKey();
+        }
+    }
+}
